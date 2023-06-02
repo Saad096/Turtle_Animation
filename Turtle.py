d2e154a67bc1a39b9bd8b9dd56d186ce8dfb7591
Saad Alam <109945767+Saad096@users.noreply.github.com>
2023-06-02 23:27:25 +0500

    Amazing fun with python using turtle module

---
+++ Turtle.py
@@ -0,0 +1,10 @@
+import turtle
+turtle.speed(100)
+turtle.bgcolor('black')
+
+for i in range(200):
+    turtle.color('yellow')
+    turtle.circle(i)
+    turtle.right(5)
+turtle.hideturtle()
+turtle.done()
